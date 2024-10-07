c3da334e103724c5b62066cc988800bf025bb109
eloisy04 <eloisy.soares@escola.pr.gov.br>
2024-10-07 16:09:40 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,11 @@
+criacartao(
+    "historia",
+    "Qual o dia a independência do Brasil?",
+    "7  setembro"
+)
+
+criacartao(
+    "geografia",
+    "Qual o país que é dividido por duas ilhas?",
+    "Nova Zelândia"
+)
